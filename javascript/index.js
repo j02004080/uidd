0b4c71d396df36133f6559f7559e4fbd92b83167
Cloudhsu <j02004080@gmail.com>
2016-03-21 17:43:09 +0800

    03-21

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $(".colorithover").hide();
     $(".again").hide();
+    $(".about2").hide();
+    $(".location2").hide();
+    $(".catagory2").hide();
+    
     $(".colorit").hover(function(){
         $(".colorit").hide();
         $(".colorithover").show();
@@ -32,9 +36,90 @@ $(document).ready(function() {
         }, 200);
     });
     
-    $("#close").click(function(){
+    $(".close").click(function(){
         $('.menu').animate({
             left:"-285px"
         }, 200);
+        $('.about').animate({
+            left:"-650px"
+            },200);
+        $('.location').animate({
+            left:"-650px"
+            },200);
+        $('.catagory').animate({
+            left:"-650px"
+            },200);
+    });
+    
+    $(".about1").hover(function(){
+        $('.about2').show();
+        $('.about1').hide();
+    });
+    $(".about2").mouseout(function(){
+        $('.about1').show();
+        $('.about2').hide();
+    });
+    
+    $(".location1").hover(function(){
+        $('.location2').show();
+        $('.location1').hide();
+    });
+    $(".location2").mouseout(function(){
+        $('.location1').show();
+        $('.location2').hide();
+    });
+    
+    $(".catagory1").hover(function(){
+        $('.catagory2').show();
+        $('.catagory1').hide();
+    });
+    $(".catagory2").mouseout(function(){
+        $('.catagory1').show();
+        $('.catagory2').hide();
+    });
+    
+    $(".about2").click(function(){
+        $('.menu').animate({
+            left:"650px"
+        },200);
+        $('.about').animate({
+            left:"0px"
+        },200);
+        $('.location').animate({
+            left:"-650px"
+        },200);
+        $('.catagory').animate({
+            left:"-650px"
+        },200);
+    });
+    
+    $(".location2").click(function(){
+        $('.menu').animate({
+            left:"650px"
+        },200);
+        $('.location').animate({
+            left:"0px"
+        },200);
+        $('.catagory').animate({
+            left:"-650px"
+        },200);
+        $('.about').animate({
+            left:"-650px"
+        },200);
+    });
+    
+    $(".catagory2").click(function(){
+        $('.menu').animate({
+            left:"650px"
+        },200);
+        $('.catagory').animate({
+            left:"0px"
+        },200);
+        $('.location').animate({
+            left:"-650px"
+        },200);
+        $('.about').animate({
+            left:"-650px"
+        },200);
     });
 });
